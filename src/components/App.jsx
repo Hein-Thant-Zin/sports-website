80225f2d26b906e9e9aaa19961e03bfabab8c00c
heinthantzin <heintz414@gmail.com>
2023-09-19 18:59:28 +0630

    using_useRoutes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 
 import NavBar from './NavBar'
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useRoutes } from 'react-router-dom'
 import Team from './Team'
 import Article from './Article'
 import { FadeLoader } from 'react-spinners';
@@ -9,8 +9,56 @@ const TeamsDetails = React.lazy(() => import('./TeamsDetails'));
 const Player = React.lazy(() => import('./Player'));
 const Home = React.lazy(() => import('./Home'));
 const Players = React.lazy(() => import('./Players'));
-const Teams = React.lazy(()=>import('./Teams'))
-const Articles = React.lazy(()=>import('./Articles'))
+const Teams = React.lazy(() => import('./Teams'));
+const Articles = React.lazy(() => import('./Articles'));
+
+function Routers() {
+  return useRoutes([
+    { path: '/', element: <Home /> },
+    {
+      path: '/players', element: <Players />,
+      children: [
+        {
+        path : ':playerId',
+        element : <Player />
+        }, {
+        path: '',
+        element : <section className='container py-16 text-center'>Choose a player..</section>
+        },
+      ],
+    },
+    {
+      path: '/teams', element: <Teams />,
+      children: [
+        {
+        path: ':teamId',
+        element: <Team />
+        }, {
+        path: '',
+        element : <section className='container py-16 text-center'>Choose a team..</section>
+      }
+      ],
+    },
+    {
+      path: '/:teamId',
+      element : <TeamsDetails />
+    },
+    {
+      path: '/:teamId/articles',
+      element: <Articles />,
+      children: [
+        {
+        path: ':teamId',
+        element : <Team />
+        }, {
+        path: '',
+        element : <section className='container py-16 text-center'>Choose an article..</section>
+      }
+      ],
+    },
+  ])
+}
+
 
 
 const App = () => {
@@ -19,22 +67,7 @@ const App = () => {
       <NavBar />
 
       <React.Suspense fallback={<FadeLoader color='#ffffff'/>}>
-        <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/players' element={<Players />}>
-          <Route path=':playerId' element={<Player />} />
-            <Route path='' element={<section className='container py-16 text-center'>Choose a player..</section>}></Route>
-        </Route>
-        <Route path='/teams' element={<Teams />} >
-          <Route path=':teamId' element={<Team />} />
-          <Route path='' element={<section className='container py-16 text-center'>Choose a team..</section>}></Route>
-        </Route>
-        <Route path='/:teamId' element={<TeamsDetails />} />
-        <Route path='/:teamId/articles' element={<Articles />}>
-          <Route path=':articleId' element={<Article />}></Route>
-          <Route path='' element={<section className='container py-16 text-center'>Choose an article..</section>}></Route>
-        </Route>
-      </Routes>
+        <Routers />
       </React.Suspense>
       
     </main>
